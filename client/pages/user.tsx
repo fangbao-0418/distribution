b8638da5a3568c0d818c11785399d78ef4435509
fangbao <fangbao@i-counting.cn>
2019-01-22 15:38:13 +0800

    [fix] 优化登陆注册 related to issue #1

--- client/pages/user.tsx
+++ client/pages/user.tsx
@@ -5,7 +5,6 @@ export default class View extends Layout {
   static async getStore({ctx}) {
     const store = this.configureStore()
     const data = await ctx.service.user.getUserInfo()
-    console.log(data, 'server user')
     store.dispatch(actions.user.info(data))
     return store
   }
