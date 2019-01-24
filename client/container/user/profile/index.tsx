51aad19b9e407874338456d7917a7f19b489b74d
fangbao <fangbao@i-counting.cn>
2019-01-24 15:02:00 +0800

    [fix] 修改个人页面 related to issue #1

--- client/container/user/profile/index.tsx
+++ client/container/user/profile/index.tsx
@@ -4,7 +4,9 @@ const cx = classnames.bind(require('./style.module.sass'))
 class Main extends React.Component {
   render () {
     return (
-      <div className={cx('bg')}></div>
+      <div className={cx('bg')}>
+        <img src={require('client/assets/label_erwi@3x.png')}/>
+      </div>
     )
   }
 }
